aca5037b1c6e53289703c746a1ef7eb7430d25ee
kevin <dqtkien@gmail.coom>
2022-03-17 15:20:08 +0700

    Split token testnet to another file

--- seeds/fantom_testnet_data.ts
+++ seeds/fantom_testnet_data.ts
@@ -5,22 +5,23 @@ export async function seed(knex: Knex): Promise<void> {
   await knex('token').del();
 
   // Inserts seed entries
+
   await knex('token').insert([
     {
-      chainId: 250,
+      chainId: 4002,
       type: 721,
       name: 'DuelistKingCard',
       symbol: 'DKC',
       decimal: 18,
-      address: '0xC44b1022f4895F3C04e965f8A82437a8B5cebB70',
+      address: '0xE1f03feAFB6107E82191CdB46f270c2ce962eC4e',
     },
     {
-      chainId: 250,
+      chainId: 4002,
       type: 721,
       name: 'DuelistKingItem',
       symbol: 'DKI',
       decimal: 18,
-      address: '0x6c375585A31718c38D4E3eb3eddbfb203f142834',
+      address: '0x8BD47c687d0D3299a71f2f9Cd9D216C2Df1271d3',
     },
   ]);
 }
